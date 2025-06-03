61c5da17585b687d9f474a50ee3309cb0b7a298b
rokas.gegevicius <rokas.gegevicius@atrandi.com>
2025-06-03 09:47:06 +0300

    initial commit - project scaffold

---
+++ src/main/java/com/rokas/portfolio/TenantTaskerApplication.java
@@ -0,0 +1,13 @@
+package com.rokas.portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenantTaskerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TenantTaskerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rokas/portfolio/TenantTaskerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rokas.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenantTaskerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
